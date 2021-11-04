e3d9fb4db0d1941a18d0a6ef7a1dd577eab83ba4
Junho Yeo <hanaro0704@gmail.com>
2021-11-04 11:58:42 +0900

    Export everything

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 export {
+  KlaytnNetwork,
+  KlaytnNetworkStage,
+  KlaytnNetworks,
+} from './KlaytnNetworks'
+
+export { KlaytnWalletOptions, KlaytnWallet } from './KlaytnWallet'
+
+export {
   KlaytnWalletProvider,
   KlaytnWalletProviderType,
 } from './KlaytnWalletProvider'
+
+export { KlaytnWalletKaikasProvider } from './KlaytnWalletKaikasProvider'
+export { KlaytnWalletKlipProvider } from './KlaytnWalletKlipProvider'
